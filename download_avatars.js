92bfa08735e8cd9929a608e7cb8c915703942c72
Darcy McLean <darc.mclean@gmail.com>
2017-10-09 20:31:15 +0000

    added download_avatars.js

---
+++ download_avatars.js
@@ -0,0 +1,8 @@
+/* 20171009 DM - LHL w2d1 GitHub Avatar Download Project.
+
+  Scoops up all the GitHub avatars and stashes them locally.
+
+*/
+var request = require('request');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
