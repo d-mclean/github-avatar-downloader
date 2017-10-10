391c987d08ef6b0b774595eba41c00a423a6d7e7
Darcy McLean <darc.mclean@gmail.com>
2017-10-10 02:06:06 +0000

    cleaned up some old code for step 5 and 6

--- download_avatars.js
+++ download_avatars.js
@@ -7,16 +7,20 @@ var request = require('request');
 
 var GITHUB_USER = "d-mclean";
 // NOTE TO SELF - REMOVE THIS TOKEN re: environment variable instead?
-var GITHUB_TOKEN = "6b726e7b0a008ab574f3a9d424c27ce25d6dc6b2";
+//var GITHUB_TOKEN = "6b726e7b0a008ab574f3a9d424c27ce25d6dc6b2";
+var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 
+var jsonResult;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
+  // Tester (remove this later!!)
   console.log(requestURL);
 
+  // Set options - only needed for user-agent for now.
   var options = {
     url: requestURL,
     headers: {
@@ -27,47 +31,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
   request(options, function (error, response, body) {
     if (!error && response.statusCode == 200) {
         // Print out the response body
-        console.log(body)
+        //console.log(body)
+         cb(body);
+    } else {
+      //console.log(response)
     }
   });
-
-// request(requestURL, function (error, response, body) {
-//   console.log('error:', error); // Print the error if one occurred
-//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', body); // Print the HTML for the Google homepage.
-// });
-// request.headers('User-Agent', 'GitHub Avatar Downloader - Student Project');
-
-//   // Using Try/Catch to handle errors.
-//   try {
-//     request.get(requestURL)
-
-//             // Set user agent to avoid 403 errors.
-//             // .set('User-Agent', 'GitHub Avatar Downloader - Student Project')
-//             // .headers('User-Agent', 'GitHub Avatar Downloader - Student Project')
-
-//             .on('error', function(err) {
-//               throw err;
-//             })
-
-//             .on('response', function (response) {                           // Note 3
-//               console.log('Response Status Code: ', response.statusCode);
-//               console.log('Response Message: ', response.statusMessage);
-//               console.log('Response Content Type: ', response.headers['content-type']);
-//               //console.log('\n Downloading ...');
-//             })
-//             .on('finish', function(body) {
-//               console.log(body);
-//             })
-
-
-
-//   } catch (e) {
-//     console.log("Error downloading file - ", e.message);
-//   }
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
+
+        //jsonResult = JSON.stringify(JSON.parse(result));
+        //console.log(jsonResult);
   console.log("Errors:", err);
   console.log("Result:", result);
 });
