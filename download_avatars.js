28adeba6666e09da62c76ec10ddf2c07209b5cd9
Darcy McLean <darc.mclean@gmail.com>
2017-10-10 05:02:58 +0000

    update for step 7 and 8

--- download_avatars.js
+++ download_avatars.js
@@ -8,15 +8,10 @@
 */
 var request = require('request');
 
-var GITHUB_USER = "d-mclean";
+var GITHUB_USER = 'd-mclean';
 // NOTE TO SELF - REMOVE THIS TOKEN re: environment variable instead?
-//var GITHUB_TOKEN = "6b726e7b0a008ab574f3a9d424c27ce25d6dc6b2";
+//var GITHUB_TOKEN = '6b726e7b0a008ab574f3a9d424c27ce25d6dc6b2';
 var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-
-var jsonResult;
-var avatarsFolder;
-var avatarFileExt;
-var avatarFP;
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -42,8 +37,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
          cb(error, body);
     } else {
       // Let the user know what happened if there's an error.
-      console.log("Errors:", err);
-      console.log("Result:", result);
+      console.log('Errors:', err);
+      console.log('Result:', result);
     }
   });
 };
@@ -51,6 +46,9 @@ function getRepoContributors(repoOwner, repoName, cb) {
 // Step 7
 function downloadImageByURL(url, filePath) {
   var fs = require('fs');
+  var avatarsFolder;
+  var avatarFileExt;
+  var avatarFP;
 
   // Get rid of the file name so we can create the directory if needed.
   avatarsFolder = filePath.substr(0, filePath.lastIndexOf('/'));
@@ -68,11 +66,6 @@ function downloadImageByURL(url, filePath) {
             })
 
             .on('response', function (response) {
-              // console.log('Response Status Code: ', response.statusCode);
-              // console.log('Response Message: ', response.statusMessage);
-              // console.log('Response Content Type: ', response.headers['content-type']);
-              //console.log('headers:', response.headers);
-
               // If it's an image, grab the file extension.
               if (response.headers['content-type'].substring(0,5) == 'image'){
                 avatarFileExt = response.headers['content-type'].substring(response.headers['content-type'].length - 3)
@@ -80,37 +73,48 @@ function downloadImageByURL(url, filePath) {
               }
 
               console.log('\n Downloading image ' + avatarFP + '...');
-
             })
 
             .pipe(fs.createWriteStream(filePath))
 
             .on('finish', function() {
-              console.log(" ... download complete (" + avatarFP + ").");
+              console.log(' ... download complete (' + avatarFP + ').');
+              // Renaming the file here because I can't get it to save it with the extension.
               fs.rename(filePath, avatarFP);
             })
 
   } catch (e) {
-    console.log("Error downloading file - ", e.message);
+    console.log('Error downloading file - ', e.message);
   }
 }
-/*
-// Tester (Steps 1 - 6):
-getRepoContributors("jquery", "jquery", function(err, result) {
-  // Parse the json object, looking for and outputting the avatar icons.
-  var obj = JSON.parse(result, function (key, value) {
 
-    if (key == "avatar_url") {
-      //console.log(value);
-      console.log(value);
+// Tester (Steps 1 - 6):
+getRepoContributors('d-mclean', 'hello-world', function(err, result) {
+  var strURL = '';
+  var strFilename = '';
+
+  // Parse the json object, looking for and outputting the avatar icons.
+  JSON.parse(result, function (key, value) {
+    //if (key == 'avatar_url') { return value; } else {return value;}
+    if (key == 'avatar_url') {
+      strURL = value;
     }
-    if (key == "login") {
-      //console.log(value);
-      console.log(value);
+    if (key == 'login') {
+      strFilename = value;
+    }
+
+    // If we have the URL and filename, then download it.
+    if (strURL !== '' && strFilename !== ''){
+      downloadImageByURL(strURL, 'avatars/' + strFilename);
+
+      // Re-initialize variables back to the empty string.
+      strURL = '';
+      strFilename = '';
     }
   })
 });
-*/
 
+/*
 // Tester (Step 7+):
 downloadImageByURL('https://avatars1.githubusercontent.com/u/109334?v=4', 'avatars/kvirani');
+*/
